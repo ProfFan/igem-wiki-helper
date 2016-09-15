1ea30a280cd3a0da0904ba1cc1e3b41cfb30c140
Fan Jiang <ProfFan@users.noreply.github.com>
2016-09-15 15:18:28 +0800

    Create upload.js

---
+++ upload.js
@@ -0,0 +1,39 @@
+function readfiles(files) {
+var formData = new FormData();
+
+formData.append('file', files[0]);
+
+formData.append('filename', 'SUSTech_Shenzhen-'+files[0].name);
+formData.append('format','json');
+formData.append('token',mw.user.tokens.get( 'editToken' ));
+
+$.ajax({
+    url: '/wiki/api.php?action=upload',
+    type: 'POST',
+    data: formData,
+    async: true,
+    success: function (data) {
+        console.log(data);
+        
+    },
+    cache: false,
+    contentType: false,
+    processData: false
+ });
+
+
+}
+
+
+var dropzone = document.getElementById('wpTextbox1'); 
+dropzone.ondrop = function(e){
+    console.log('drop'); 
+    e.preventDefault();
+    readfiles(e.dataTransfer.files);
+    e.dataTransfer.setData("text/plain", e.dataTransfer.files[0].name);
+    var $txt = jQuery(dropzone);
+        var caretPos = $txt[0].selectionStart;
+        var textAreaTxt = $txt.val();
+        var txtToAdd = "[[File:SUSTech_Shenzhen-"+e.dataTransfer.files[0].name+"]]";
+        $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+};
